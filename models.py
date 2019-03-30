43dd55da63de18bac5d96a86b96ecc047edaacd5
Dipesh <dipeshsukhani@gmail.com>
2019-03-30 20:11:04 +0800

    Models.py
    Updated the file for creating the engine for the DB

--- models.py
+++ models.py
@@ -1,15 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String
 # from app import db
 
-engine = create_engine('sqlite:///database.db', echo=True)
+
+user_database_url = environ.get('DATABASE_URL' secret.user_database_url)
+engine = create_engine(user_database_url)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+
+# DKSukh to look into the following function ----->
+
 Base = declarative_base()
 Base.query = db_session.query_property()
+
+# ----->
+
 
 # Set your classes here.
 
